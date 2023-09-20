6db30c3fcdd6c15dd914909dcdf1421359e047a4
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-09-21 04:09:46 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -21,7 +21,7 @@ import time
 import threading
 
 class Sultaan (Robot):
-    SMALLEST_TURNING_RADIUS = 0.1 #0.1
+    SMALLEST_TURNING_RADIUS = 0 #0.1
     SAFE_ZONE = 0.75
     TIME_BEFORE_DIRECTION_CHANGE = 80   # 80
     k=0
@@ -80,9 +80,9 @@ class Sultaan (Robot):
             
             if(self.fall_detector.detect_fall()): 
                 self.fall = True
-            if 0.3 < t < 5:
+            if 0.3 < t < 3:
                 self.start_sequence()
-            elif t > 5:
+            elif t > 3:
                 self.fall_detector.check()
                 
                 d = self.getDistance()
