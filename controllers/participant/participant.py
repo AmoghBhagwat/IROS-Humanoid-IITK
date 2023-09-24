fdc2d3987ad39171c0d4b60304991b619cc9abac
Amogh Bhagwat <27415139+AmoghBhagwat@users.noreply.github.com>
2023-09-24 23:52:06 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -203,12 +203,8 @@ class Sultaan (Robot):
 
         self.library.play('Khushi2')
         angle = 0
-        if normalized_x > 0:
-            angle = 3.14 / 4
-        elif normalized_x < 0:
-            angle = -3.14/4
-        else:
-            return
+        if abs(normalized_x) > 0.6:
+            angle = 3.14 / 5 
             
         self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=angle)
 
